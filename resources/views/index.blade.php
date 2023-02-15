f58bc8a2ba9f07b3f4dbb1d75ecd077ee29bd229
Сергей Дружинин <yamamoto@MacBook-Pro-Sergej.local>
2023-02-15 11:35:14 +0300

    fix view index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,14 @@
 
 <div class="mr-auto place-self-center lg:col-span-7">
   <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-    {{ __('Привет от хекслета!') }}
+    {{ __('Hello from Hexlet!') }}
   </h1>
   <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-    {{ __('Это простой менеджер задач на Laravel') }}
+    {{ __('Practical programming courses') }}
   </p>
   <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
     <a href="https://hexlet.io" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" target="_blank">
-      {{ __('Нажми на меня') }}
+      {{ __('Learn more') }}
     </a>
   </div>
 </div>
