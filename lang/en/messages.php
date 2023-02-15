f94d92e81be88dbd3cf5a3487da4cc4924c676ff
Сергей Дружинин <yamamoto@MacBook-Pro-Sergej.local>
2023-02-15 11:16:34 +0300

    fin step

---
+++ lang/en/messages.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+  'status' => [
+    'created' => 'Status was created successfully',
+    'updated' => 'Status was updated successfully',
+    'deleted' => 'Status was deleted successfully',
+    'unsuccessful' => 'The attempt to delete the status failed'
+  ],
+  'label' => [
+    'created' => 'Label was created successfully',
+    'updated' => 'Label was updated successfully',
+    'deleted' => 'Label was deleted successfully',
+    'unsuccessful' => 'The attempt to delete the label failed'
+  ],
+  'task' => [
+    'created' => 'Task was created successfully',
+    'updated' => 'Task was updated successfully',
+    'deleted' => 'Task was deleted successfully',
+    'unsuccessful' => 'The attempt to delete the task failed'
+  ]
+];
